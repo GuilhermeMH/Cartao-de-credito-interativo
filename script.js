0b69f23d394f8c4792e922b9b5ad735e9d414cab
Guilherme Moura <104610969+GuilhermeMH@users.noreply.github.com>
2023-04-11 16:49:29 -0300

    Iniciando arquivo JS

---
+++ script.js
@@ -0,0 +1,15 @@
+const cardCVC = document.querySelector(".cvc span");
+const cardNumber = document.querySelector(".card-Number");
+const cardName = document.querySelector(".cardholder-name");
+const cardExpDate = document.querySelector(".exp-date");
+
+const form = document.querySelector("form");
+const inputName = document.querySelector("#name");
+const inputNumber = document.querySelector("#card-number");
+const inputMonth = document.querySelector("#month");
+const inputYear = document.querySelector("#year");
+const inputCVC = document.querySelector("#cvc");
+
+const infoErr = document.querySelectorAll(".info-err");
+const complete = document.querySelector(".complete");
+
